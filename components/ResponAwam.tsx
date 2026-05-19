508ccb82a0d4cbf1e92b35462a0b2b515cd7debb
gunbladeii <gunbladeii25@gmail.com>
2026-05-19 10:09:34 +0800

    content: update text in ResponAwam component

--- components/ResponAwam.tsx
+++ components/ResponAwam.tsx
@@ -262,7 +262,7 @@ export default function ResponAwam() {
                 {/* Name + Jawatan */}
                 <div className="grid grid-cols-1 sm:grid-cols-2 gap-3">
                   <div>
-                    <label className="text-xs font-semibold text-gray-500 block mb-1">Nama Penuh *</label>
+                    <label className="text-xs font-semibold text-gray-500 block mb-1">Nama Penuh/Lokasi Kerja/Jawatan*</label>
                     <input
                       type="text"
                       value={form.nama}
